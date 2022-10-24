458c2a4d95900cde6f166a181e3835e5aea12410
Shohieb <shohiebsense@users.noreply.github.com>
2022-10-24 18:59:46 +0900

    Initial Commit

---
+++ Shared/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Shared
+//
+//  Created by Nasruddin, Shohieb | LIPD on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
