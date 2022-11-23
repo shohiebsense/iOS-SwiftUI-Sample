d5504148884a6d2a5be54abb6e1f3e0e72bd6f36
Shohieb <shohiebsense@users.noreply.github.com>
2022-11-23 13:56:37 +0900

    feat/add-TestData.swift

---
+++ Shared/Resources/TestData.swift
@@ -0,0 +1,19 @@
+//
+//  TestData.swift
+//  Sample (iOS)
+//
+//  Created by Nasruddin, Shohieb | LIPD on 2022/11/23.
+//
+
+import Foundation
+
+struct TestData {
+    static var Questions: [Question] = {
+        let url = Bundle.main.url(forResource: "Questions.json", withExtension: nil)!
+        let data = try! Data(contentsOf: url)
+        let wrapper = try! JSONDecoder().decode(Wrapper<Question>.self, from: data)
+        return wrapper.items
+    }()
+    
+    static let user = User(name: "Lumir Sacharov", reputation: 2345, profileImageURL: nil)
+}
